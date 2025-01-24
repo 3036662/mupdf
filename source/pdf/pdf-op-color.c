b2ba5c0ff3ec0fc95a6fc0df3f3329a1671dacd6
Oleg Proskurin <proskur@altlinux.org>
2025-01-24 11:46:44 +0300

    Remove dereferencing the 'stk' variable after free.

--- source/pdf/pdf-op-color.c
+++ source/pdf/pdf-op-color.c
@@ -1689,7 +1689,8 @@ pdf_color_push_resources(fz_context *ctx, pdf_processor *proc, pdf_obj *res)
 		pdf_drop_obj(ctx, stk->old_rdb);
 		pdf_drop_obj(ctx, stk->new_rdb);
 		fz_free(ctx, stk);
-		p->rstack = stk->next;
+		// dereferencing the `stk` variable after free(stk)
+		//p->rstack = stk->next;
 		fz_rethrow(ctx);
 	}
 }
